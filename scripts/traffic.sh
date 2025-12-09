81db2980d01a748facd7f73f41bcad2dc66371c0
zhmac <1815526897@qq.com>
2025-12-09 16:20:52 +0800

    first commit

--- scripts/traffic.sh
+++ scripts/traffic.sh
@@ -48,6 +48,7 @@ do
     --patience 10 \
     --train_epochs 100 \
     --devices 0,1,2,3,4,5,6,7 \
+    --use_multi_gpu \
     --alpha $alpha \
     --learning_rate 0.0005 \
     --itr 1 | tee logs/test/new/$data_name'_'$alpha'_'$model_name'_'$pred_len.logs
